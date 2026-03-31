e9507f8454a7383f3a4ff19ef1825f5859c5736d
HuhSeungKyu <l1o1s1e1@hanmail.net>
2026-04-01 01:21:47 +0900

    url 변경

--- src/pages/ProductAll.jsx
+++ src/pages/ProductAll.jsx
@@ -6,7 +6,7 @@ import { Col, Container, Row } from 'react-bootstrap';
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
   const getProducts = async() => {
-    let url = 'http://69cbed010b417a19e07b846e.mockapi.io/shopmall/products';
+    let url = 'https://69cbed010b417a19e07b846e.mockapi.io/shopmall/products';
     let res = await fetch(url);
     let data = await res.json();
 console.log(11)
